291aee1dafccac212a981e58cc338f47c5296005
srmward <srmward@gmail.com>
2019-05-27 11:35:54 -0700

    Convert Let to Const where Mutation Not Expected

--- src/controllers/boxes.ts
+++ src/controllers/boxes.ts
@@ -2,5 +2,5 @@ import { Request, Response } from 'express'
 import Box from '../models/Box'
 import IBoxModel from '../types/IBoxModel'
 
-export let getBoxes = (req: Request, res: Response) =>
+export const getBoxes = (req: Request, res: Response) =>
   Box.find((err: Error, doc: IBoxModel[]) => res.send(doc))
--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -4,5 +4,5 @@ import { Request, Response } from 'express'
  * GET /
  * Base route
  */
-export let index = (req: Request, res: Response) =>
+export const index = (req: Request, res: Response) =>
   res.send('Welcome to the Inventory API')
