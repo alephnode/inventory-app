ce7f9e7f5bacce18b96420650a3590f71a3bac7c
srmward <srmward@gmail.com>
2019-05-27 11:29:17 -0700

    Update Type in Box Controller

--- src/controllers/boxes.ts
+++ src/controllers/boxes.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express'
 import Box from '../models/Box'
+import IBoxModel from '../types/IBoxModel'
 
-// TODO: Fix doc type
 export let getBoxes = (req: Request, res: Response) =>
-  Box.find(function(err: Error, doc: any) {
+  Box.find(function(err: Error, doc: IBoxModel[]) {
     return res.send(doc)
   })
