3bc9bcdb829f232ea5dc7c146ac667aac8b15510
srmward <srmward@gmail.com>
2019-06-21 11:19:18 -0600

    Remove Needless Webpack Plugin, Add Build Script

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,6 @@
 const webpack = require('webpack')
 const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   context: resolve(__dirname, 'src'),
