d266e740479c6fc13492485485bdf170dbaa03d7
srmward <srmward@gmail.com>
2019-06-23 22:11:13 -0700

    Fun Header Design

--- frontend/src/common/styles/index.js
+++ frontend/src/common/styles/index.js
@@ -13,6 +13,8 @@ const headerStyles = css`
   font-size: 1.8rem;
   position: fixed;
   top: 16px;
+  text-shadow: -3px 3px 0px var(--hover-color),
+    -1px 1px 0px var(--primary-bg-color);
 `
 
 export { baseStyles, headerStyles }
