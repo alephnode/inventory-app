f8a2014d61775b746f66e4ba37004337f019dd75
srmward <srmward@gmail.com>
2019-08-07 21:23:23 -0700

    Add Request Test

--- backend/tests/boxes.test.ts
+++ backend/tests/boxes.test.ts
@@ -1,6 +1,14 @@
 import { stripLetters } from '../src/controllers/boxes'
+import request from 'supertest'
+import app from '../src/app'
 
 test('Should strip whitespace characters', () => {
   const result: number = stripLetters('A342')
   expect(result).toBe(342)
+})
+
+test('Get the boxes', async () => {
+  await request(app)
+    .get('/boxes')
+    .expect(200)
 })
